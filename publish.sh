32fbc8026818be9448d96e460d791a24c51126a8
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-08-25 14:55:11 +0200

    Site updated: 2018-08-25 14:55:07

--- publish.sh
+++ publish.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-echo "Do you really want to publish (y/n)? "
-
-read i_am_sure
-
-if [ "$i_am_sure" != "y" ]; then
-  echo -e "\nok, bye!"
-  exit 0
-fi
 
 git submodule update --remote public
 pushd public
@@ -32,15 +22,6 @@ pushd public
   git commit -m "Site updated: $now"
   git push origin master
 popd
-
-echo -e "\nDone! Do you want to update the sources, too?"
-read i_am_sure
-
-if [ "$i_am_sure" != "y" ]; then
-  echo -e "\nok, bye!"
-  exit 0
-fi
-
 
 git add .
 git commit -m "Site updated: $now"
